93d19410acd4c1dca4e8234de05279d3093dd8e9
katherin <katheringabriela2310@gmail.com>
2021-08-12 22:40:30 -0600

    change

--- DevBookStore/settings.py
+++ DevBookStore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c4%-6pi2o3g9i)y7kw8f9z*u*@b4s55esnv()46l=w71h+8xru'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
