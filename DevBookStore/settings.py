bef5e435e85106fa3d90e2a605d82149e957fedd
katherin <katheringabriela2310@gmail.com>
2021-09-16 12:44:30 -0600

    tamaño

--- DevBookStore/settings.py
+++ DevBookStore/settings.py
@@ -91,12 +91,12 @@ DATABASES = {
 
 
 
-#DATABASES = {
-#    'default': {
-#       'ENGINE': 'django.db.backends.sqlite3',
-#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 import dj_database_url
@@ -107,6 +107,7 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
+
 
 
 # Password validation
