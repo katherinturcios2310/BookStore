7ce69599b0212f8c19311ee26bce8404105cdf39
katherin <katheringabriela2310@gmail.com>
2021-09-25 18:04:01 -0600

    Link

--- DevBookStore/settings.py
+++ DevBookStore/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
     }
 }
 
-
+"""
 import dj_database_url
 from decouple import config
 
@@ -108,7 +108,7 @@ DATABASES = {
     )
 }
 
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
